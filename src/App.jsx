2972f6ba8270f56ec32ebac670b45fdd8e22f50d
Mohit kushwah <besharamcode@gmail.com>
2024-06-04 22:47:55 +0530

    changes commite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
